8897a6e439622808e9773a090084f561cc37d038
HanHyeJi <hjhan.0621@gmail.com>
2023-09-13 21:27:10 +0900

    feat: PrincipalDetails 구현

---
+++ src/main/java/com/cos/security1/auth/PrincipalDetails.java
@@ -0,0 +1,68 @@
+package com.cos.security1.auth;
+
+import com.cos.security1.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+// 시큐리티가 /login 주소 요청을 낚아채 로그인을 진행
+// 로그인이 완료되면 시큐리티 session을 만듬 (Security ContextHolder)
+// 오브젝트 => Authentication 타입 객체
+// Authentication 안에 User 정보가 있어야 함
+// User 오브젝트 타입 => UserDetails 타입 객체
+
+// Security Session => Authentication => UserDetails
+public class PrincipalDetails implements UserDetails {
+
+    // 컴포지션
+    private User user;
+
+    public PrincipalDetails(User user) {
+        this.user = user;
+    }
+
+    // 해당 User의 권한을 리턴하는 곳
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collect = new ArrayList<>();
+        collect.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return user.getRole();
+            }
+        });
+        return collect;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
