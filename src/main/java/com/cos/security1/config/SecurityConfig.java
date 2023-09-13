c124841a15e8c7e3acd20c6d8a8e417fb9a3737e
HanHyeJi <hjhan.0621@gmail.com>
2023-09-13 16:53:41 +0900

    feat: 시큐리티 기본설정

---
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.cos.security1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 필터가 스트링 필터체인에 등록됨
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+
+                // 아래 주소 접속 시 인증 필요
+                .antMatchers("/user/**").authenticated()
+
+                // 아래 주소 접속 시 인증 + admin or manager 권한 필요
+                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+
+                // 아래 주소 접속 시 인증 + admin 권한 필요
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+
+                // 그 이외 주소 접속 시 인증 필요 X
+                .anyRequest().permitAll()
+
+                // 권한이 없는 페이지로 접속 시 로그인 주소("/login")로 이동할 수 있도록 설정
+                .and()
+                .formLogin()
+                .loginPage("/login");
+    }
+}
